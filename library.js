cbb6c019f1c01865dfa6b1321441b87845769958
Lucas-Dyck <lucasedyck@gmail.com>
2026-07-06 17:39:54 -0400

    Create library.js

---
+++ library.js
@@ -0,0 +1,173 @@
+export const songLibrary = [
+    {album: "Shane and Shane: The Worship Initiative",
+    songsArray: ["God of Ages Past", "Seas of Crimson", "This I Believe (The Creed)", "Scandal of Grace", "Forever (We Sing Hallelujah)", "Jesus Loves Me", "Christ Be All Around Me", "You Make Me Brave", "Great Are You Lord", "Man of Sorrows"]
+    },
+    {album: "The Worship Initiative Volume One",
+    songsArray: ["Oceans", "This is Amazing Grace", "All the Poor and Powerless", "Lord I Need You", "In Christ Alone (Passion)", "Beautiful Things", "Come Thou Fount (Vol. 1)", "10,000 Reasons", "Whom Shall I Fear (God of Angel Armies)", "Happy Day"]
+    },
+    {album: "The Worship Initiative Volume Two", 
+    songsArray:["Heart Starts Beating", "Hosanna (Praise Is Rising)", "Forever Reign", "Mercy", "How Great is Our God", "You Are My All in All", "We Exalt Thee", "Our God", "Open the Eyes of my Heart",  "Better is One Day"]
+    },
+    {album: "The Worship Initiative Volume Three",
+    songsArray: ["So Can I Love", "I Will Follow", "Liberty", "Lord, You're Beautiful", "How He Loves" , "Never Once", "Shout to the Lord", "Before the Throne of God Above", "Beautiful One", "Blessed Be Your Name"]
+    },
+    {album: "The Worship Initiative Volume Four", 
+    songsArray: ["I Stand Amazed (How Marvellous)", "Our God is Love", "Holy is the Lord", "Here for You", "Always", "Here I Am To Worship", "Worthy of Affection", "Healer", "It is Well (Hymn)", "O Praise Him (All This For a King)"]
+    },
+    {album: "The Worship Initiative Volume Five",
+    songsArray: ["Cornerstone","Reign in Us", "Cannons", "Though You Slay Me", "Let My Words Be Few", "I Am Set Free", "Give Us Clean Hands", "Jesus Paid It All", "Your Great Name", "The Wonderful Cross"]
+    },  
+    {album: "The Worship Initiative Volume Six",
+    songsArray: ["Because He Lives", "Now and Always", "Revelation Song", "Great is Thy Faithfulness", "The Cross Stands", "Marvellous Light", "Forever(Sing Praise)", "One Thing Remains", "Lead Me to the Cross", "Your Grace is Enough"]
+    },
+    {album: "The Worship Initiative Volume Seven",
+    songsArray: ["You're Beautiful", "Hosanna", "Mighty to Save", "Glorify Thy Name", "Desert Song", "Holy and Anointed One", "Where Would I Be", "Everything is Different", "Everlasting God", "I Will Exalt You"]
+    },
+    {album: "The Worship Initiative Volume Eight", 
+    songsArray: ["The Stand", "Indescribable", "I Give You My Heart", "Nothing But The Blood", "No Sweeter Name", "You Never Let Go", "Enough", "Amazing Grace (My Chains are Gone)", "Alive Again", "Yearn"]
+    },
+    {album: "The Worship Initiative Volume Nine",
+    songsArray: ["Your Love Is Strong", "I'm Yours(Nothing But the Blood)", "Heart of Worship", "From The Inside Out", "Stronger", "That's How You Forgive", "Christ is Risen", "He is Exalted", "When the Stars Burn Down", "Your Name"]
+    },    
+    {album: "The Worship Initiative Volume Ten", 
+    songsArray: ["God Is Able", "Your Love Never Fails", "Your Grace Finds Me", "I Found It All", "Majesty", "Jesus Messiah", "I Will Rise", "God of Wonders", "You Are My King (Amazing Love)", "How Deep the Father's Love for Us"]
+    },
+    {album: "The Worship Initiative Volume Eleven",
+    songsArray: ["O Come To The Altar", "Open Up the Heavens", "Be Enthroned", "Good Good Father", "Holy Spirit", "No Longer Slaves", "At The Cross (Love Ran Red)", "Praise The King", "Your are My King (Amazing Love)", "Sovereign Over Us", "Trust You"]
+    },
+    {album: "The Worship Initiative Volume Twelve", 
+    songsArray: ["You're Worthy of It All", "Jesus We Love You", "Lion and The Lamb", "Alive", "Ever Be (Girls)", "Ever Be (Guys)", "What a Beautiful Name", "Once For All", "Come As You Are", "The Lord Our God", "Even So Come", "Great I Am", "O Praise The Name"]
+    },
+    {album: "The Worship Initiative Volume Thirteen",
+    songsArray: ["How Can It Be", "Love So Great", "Glorious Day", "King of My Heart", "God I Look To You"]
+    },
+    {album: "The Worship Initiative Volume Fourteen", 
+    songsArray: ["Worthy of Your Name", "Build My Life", "Lamb of God", "Here's My Heart", "Death Was Arrested"]
+    },
+    {album: "The Worship Initiative Volume Fifteen",
+    songsArray: ["Reckless Love", "Your Love Awakens Me", "Tremble", "Christ Is Enough", "Resurrecting"]
+    },
+    {album: "The Worship Initiative Volume Sixteen",
+    songsArray: ["Living Hope", "Who Say I Am", "Life Defined", "Do It Again", "Let It Be Jesus", "So Will I", "Behold (Then Sings My Soul)", "Look To The Son", "Chain Breaker", "This We Know"]
+    },
+    {album: "The Worship Initiative Volume Seventeen",
+    songsArray: ["Goodness of God", "Touch the Sky", "Lean Back", "Give Us Your Heart", "Isn't He", "It Is Well", "Jesus Is Better", "New Wine", "I Shall Not Want", "Holy Ground", "Defender", "Broken Vessels"]
+    },
+    {album: "The Worship Initiative Volume Eighteen",
+    songsArray: ["All Hail King Jesus", "Yet Not I But Through Christ in Me", "How Great Is Your Love", "King of Kings", "Jesus You Alone", "Stand In Your Love", "The Way (New Horizon)", "Yes I Will", "Hallelujah For The Cross", "Great Things"]
+    },
+    {album: "The Worship initiative Volume Nineteen",
+    songsArray: ["Another In The Fire", "Alabaster Heart", "Yes And Amen", "Heart of God", "Only A Holy God", "Christ is Mine Forevermore", "All I Have Is Christ", "Faithful", "Blessed Assurance", "King of Glory"]
+    }, 
+    {album: "The Worship Initiative Volume Twenty",
+    songsArray: ["Behold The Lamb", "The Passion", "You Cannot Be Stopped", "His Heart Beats", "Jesus, Only Jesus", "Christ Our Hope In Life and Death", "My Victory", "Glorious Day (Living He Loved Me)", "Overcome", "Christ is Risen"]
+    },
+    {album: "The Worship Initiative Volume Twenty-One",
+    songsArray: ["There's A River", "Doxology/Amen", "Graves Into Gardens", "What A Friend We Have in Jesus", "Sing To Jesus", "Faithful To the End", "Hymn of Surrender", "I Surrender All (All I Am)", "There's Nothing That Our God Can't Do", "The Blessing"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Two",
+    songsArray: ["Come Ye Sinners", "New Jerusalem", "Promises", "Nothing Without You", "Such An Awesome God", "At Your Name", "You Keep On Getting Better", "The Story I'll Tell", "Jesus, Firm Foundation","Crowns", "My Truest Praise"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Three",
+    songsArray: ["Where You Are", "Run To The Father", "You Keep Hope Alive", "Better", "Miracle Or Not", "No One Ever Cared For Me Like Jesus", "By The Grace of God", "Good God", "Make Room", "Psalm 13"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Four",
+    songsArray: ["Wait For You", "The Love of God", "Counting Every Blessing", "First Love Fire", "First Love/Obsession", "Abide With Me", "Holy/There Is None Like You", "Battle Belongs", "Hymn of The Holy Spirit", "Oh The Cross"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Five",
+    songsArray: ["Catch Me Singing", "Worthy of It All", "Cast My Cares (My Portion)", "Fall Afresh", "Way Maker", "As The Deer (Full Band)", "He's Always Been Faithful", "Waiting Here For You", "My Heart Is Yours", "The Blessing"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Six",
+    songsArray: ["The Greatness Of Our God", "We Praise You", "I Will Exalt You", "Famous For (I Believe)", "All Praise (Sing Praise)", "God of Revival", "Redeemed By the Blood of the Lamb", "God, You're So Good", "We Believe", "God So Loved"]
+    }, 
+    {album: "The Worship Initiative Volume Twenty-Seven",
+    songsArray: ["House of The Lord", "Highlands (Song of Ascent)", "Come in Close", "Hymn of Heaven", "The Cup Was Not Removed", "It Was Finished Upon That Cross", "Divine Exchange", "God of Israel", "A Mighty Fortress Is Our God", "One Day (When We All Get to Heaven)"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Eight", 
+    songsArray: ["Reign Above It All", "Behold Him", "Psalm 90", "None But Jesus", "As You Find Me", "Hallelujah My Soul Sings", "Abba", "Sinking Deep", "Worthy, You Are Worthy", "King Forevermore (God The Uncreated One)"]
+    },
+    {album: "The Worship Initiative Volume Twenty-Nine",
+    songsArray: ["Evidence", "Hallelujah's Song (Psalm 103)", "Drenched In Love", "Falling In Love", "Only There[Live]", "God is Love", "Found It All", "Since Your Love", "Praise the Lord (Psalm 150)", "Thank You Jesus For The Blood", "Trinity Song"]
+    },
+    {album: "The Worship Initiative Volume Thirty",
+    songsArray: ["1,000 Names", "Stronghold", "Gratitude", "Peace", "Come, Lord Jesus, Come", "Refuge"]
+    },
+    {album: "Psalms II",
+    songsArray: ["Psalm 46(Lord of Hosts)", "Psalm 45 (Fairest of All)", "Psalm 51 (Wisdom in The Secret Heart)", "Psalm 139 (Far Too Wonderful)", "Psalm 98 (Sing Unto The Lord)", "Psalm 91 (On Eagles' Wings)", "Psalm 16 (Better Than Life)", "Take Over", "Psalm 27 (One Thing)", "Psalm 23 (Surely Goodness, Surely Mercy)", "Psalm 34 (Taste and See)"]
+    },
+    {album: "Psalms From the Well",
+    songsArray: ["Yahweh (Psalm 84)", "For God Alone (Psalm 62)", "Awake My Soul (Psalm 57)", "Never Stop Singing(Psalm 95)", "More Precious Than Gold (Psalm 19)", "In This I Will Rejoice (Psalm 16)", "Bless the Lord (Psalm 103)", "I Will Dwell (Psalm 84)", "Not To Us (Psalm 115)", "Keeper of My Soul (Psalm 121)", "In His Hands(Psalm 31)", "Create In Me (Psalm 51)"]
+    },
+    {album: "Modern Hymns",
+    songsArray: ["Come Unto Jesus", "I Set My Hope One Jesus", "God Of Every Grace", "Speak O Lord", "All Sufficient Merit", "The Lord Almighty Reigns", "Rejoice", "I Am Not My Own", "All My Boast Is In Jesus", "Remember Him"]
+    },
+    {album: "Behold: The Story",
+    songsArray: ["Holy: Song of Ages", "Lament", "Simple Pursuit", "Repent (What A Joy It Is)", "Rock of Ages", "More Like Jesus", "Leaning", "We Will Feast In the House of Zion", "Homecoming", "Because He Lives"]
+    },  
+    {album: "Sunday Setlist #1",
+    songsArray: ["There Is A Saviour", "Oh, But God", "I Speak Jesus", "To The Water", "Bigger Than I Thought", "Give Thanks", "No Height, No Depth", "Thy Mercy, My God", "Heart Abandoned", "Agnus Dei"]
+    },
+    {album: "Flourish",
+    songsArray: ["Firm Foundation (He Won't)", "All Sufficient Merit", "Endless Alleluia(We Fall Down)", "Still", "You Deserve It All", "All Hail King Jesus", "You Deserve It All", "Closer", "Desert Places", "Uncomplicated", "Abide"]
+    },
+    {album: "Psalms From the Well Volume 2",
+    songsArray: ["As For Me (Psalm 73)", "Teach Me to Number My Days (Psalm 90)", "Be Still (Psalm 46)", "My Soul (Psalm 62)", "Praise Him Again (Psalm 42)", "God of Jacob (Psalm 24)", "Take Heart(Psalm 27)", "The Lord is My Light (Psalm 27)", "Yahweh, the Lord (Psalm 145)"]
+    },
+    {album: "Worship Initiative Christmas Volume 2",
+    songsArray: ["Come Thou Long Expected Jesus", "Joy to the World (Joyful, Joyful)", "Hark the Herald Angels Sing", "O Little Town of Bethlehem", "Breath of Heaven", "Face of God", "What Child is This", "Away In a Manger", "Winter Snow", "It Came Upon A Midnight Clear"]
+    },
+    {album: "The Worship initiative Christmas Volume 1",
+    songsArray: ["Little Drummer Boy", "First Noel", "O Come O Come Emmanuel", "Love Comes Down", "Angels We Have Heard On High", "Joy to the World", "Go Tell It On the Mountain", "Silent Night", "Oh Holy Night", "O Come All Ye Faithful"]
+    },
+    {album: "The Worship Initiative Christmas Volume",
+    songsArray: ["O Come, O Come, Emmanuel",  "Sing We The Song of Emmanuel", "O Holy Night", "The First Noel", "Little Drummer Boy"]
+    },
+    {album: "Psalms from the Well Deluxe Edition",
+    songsArray: ["Yahweh (Psalm 84)", "More Precious Than Gold (Psalm 19)", "In His Hands (Psalm 31)", "Awake My Soul (Psalm 57)", "Never Stop Singing (Psalm 95)", "Bless the Lord (Psalm 103)", "For God Alone (Psalm 62)", "In This I Will Rejoice (Psalm 16)", "I Will Dwell (Psalm 84)"]
+    },
+    {album: "Last Words: A Good Friday Liturgy",
+    songsArray: ["The Last Words of Jesus", "Last Words (Tenebrae)", "XI Crucifixion: Jesus Is Nailed to the Cross", "Sing to Jesus", "Isaiah 53:4-8", "Beautiful Scandalous Night", "The Cross You Bear", "All Sufficient Reward", "The Due Reward (A Doxology)"]
+    },
+    {album: "Psalms From the Well Volume 3",
+    songsArray: ["Revive Us Again (Psalm 44)", "Save Me (Psalm 3)", "Oh My Soul, Bless The Lord (Psalm 103)"]
+    },
+    {album: "Sunday Setlist #3",
+    songsArray: ["I’ve Got Joy", "Honey In the Rock", "This is Our God", "Take You At Your Word", "Holy Forever", "Jireh", "My Testimony", "Same God", "My Jesus", "Thousand Hallelujahs"]
+    }, 
+    {album: "The Worship Initiative Hymns",
+    songsArray: ["Just As I Am", "Rock of Ages", "Oh Love That Will Not Let Me Go", "The King of Love My Shepherd Is", "Fairest Lord Jesus", "Breathe On Me Breath of God", "This is My Father's World", "O For A Thousand Tongues To Sing"]
+    },
+    {album: "Sunday Setlist #4",
+    songsArray: ["The Lord's Prayer (It's Yours)", "Gratitude", "Ancient Gates", "Gloria Patri", "Back to Life", "Only King Forever", "Worthy", "Be Glad", "Egypt", "My King Forever"]
+    },
+    {album: "Acoustic Worship",
+    songsArray: ["Bless God", "Son of Suffering", "I Don't Have Much",  "I've Witnessed It", "I Need Thee Every Hour", "Trust In God", "Thousand Hallelujahs"]
+    },
+    {album: "Worship Initiative Hymns Volume 2",
+    songsArray: ["Come Thou Fount of Every Blessing", "Turn Your Eyes Upon Jesus", "All Creatures of Our God and King", "Be Thou Vision", "To God Be the Glory", "How Great Thou Art", "Crown Him (Majesty)"]
+    },
+    {album: "Advent",
+    songsArray: ["Let All Mortal Flesh Keep Silence"]
+    },
+    {album: "Advent No. 2",
+        songsArray: ["Savior of the Nations of Come"]
+    },
+    {album: "All Glory Be To Christ",
+        songsArray: ["All Glory Be To Christ"]
+    },
+    {album: "Watermark Worship Channel",
+        songsArray: ["Only Christ", "You've Already Won (Live)", "In Christ Alone [Hymns]", "Is He Worthy", "All Sufficient Merit", "O  Praise The Name (Anástasis)", "Bless God", "Holy Forever", "Who Else"]
+    },
+    {album: "We Sing To You, Jesus (Carols For Worship)",
+        songsArray: ["Joy To The World", "O Come, O Come, Emmanuel", "Come Thou Long Expected Jesus", "O Holy Night", "Hark the Herald Angels Sing", "Angels We Have Heard On High", "O Come, All Ye Faithful", "Silent Night", "The First Noel", "Go Tell It On the Mountain"]
+    },
+    {album: "WI Lite",
+        songsArray: ["Raise A Hallelujah (Lite)", "Peace (Lite)", "Bigger Than I Thought (Lite)", "Sinking Deep (Lite)", "Psalm 145 (Lite)", "Psalm 13 (Lite)", "The One You'll Find (Lite)", "Great Reward (Lite)", "Run"]
+    },
+    {album: "Worship In the Word",
+        songsArray: ["First Things First (Matthew 6)", "Your Ways (Isaiah 55)", "Tell the Story (Psalm 8)", "Come and See (Psalm 66)", "Yes and Amen (Ephesians 1)", "You Know Everything (Psalm 139)", "Holy (1 Samuel 2:2)", "Take Heart (John 16:33)", "Wholly Yours (Ephesians 3:18-19)", "Oh We Love You (John 3:16)"]
+    },
+    {album: "Worship In the Word 2",
+        songsArray: ["Infinite Love of Christ (Ephesians 3:18-19)", "Help Me Live In Step (Galatians 5:22-26)", "Sing Hallelujah (Psalm 103)", "Clap Your Hands (Psalm 47)", "I Delight In You (Psalm 1)", "Know You As You Are (Matthew 7:21-23)", "Your Amazing Grace (1 Timothy 1:15-16)", "Unto Him (Jude 1:24-25)", "Kind Like You (Ephesians 4:32)", "Fight Song (2 Chronicles 20:17)"]
+    },
+];
