5d0539e732a260d052c7c4bbd05e329ab7d9d448
Lucas-Dyck <lucasedyck@gmail.com>
2026-07-31 13:52:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ function processURL(song) {
     if (processedSong === "forever-we-sing-hallelujah") {
         processedSong = "forever-we-sing-halleluia";
     }
+    if (processedSong === "heart-of-god) {
+        processedSong += "-7a2a9346-717e-4ea1-88bc-7929bb275c74";
+    }
     var url = `https://app.theworshipinitiative.com/songs/${processedSong}/instrument/chart`;
     document.querySelector("a").setAttribute("href", url);
 }
