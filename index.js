0d92480929ec7318c5962b4c9b876772a7c4ba3c
Lucas-Dyck <lucasedyck@gmail.com>
2026-08-07 15:13:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,38 +22,36 @@ function pickSong(songLibrary){
     }
     return {song: song, album: album};
 }
-console.log(choice);
 button.addEventListener("click", function() {
-    handleClick(choice);
+    handleClick(songLibrary);
 });
 
-var choice = pickSong(songLibrary);
-
-function handleClick(choice) {
+function handleClick(songLibrary) {
+    var choice = pickSong(songLibrary);
     document.getElementById("song").textContent = choice.song;
     document.getElementById("album").textContent = choice.album;
     processURL(choice);
-} 
-
-function processURL(choice) {
-    var processedSong = (choice.song).replace(/[-\/:',()[\] ]/g, '-').replace(/-+/g, '-').replace(/^-+|-+$/g, '').toLowerCase();
-    switch (processedSong) {
-        case "all-glory-be-to-christ":
-            processedSong += "-8aeb1bd6-6af8-4a8c-90e7-b8dfe768eec3";
-            break;
-        case "there-s-a-river":
-            processedSong += "-31a29457-cb2c-4538-a81b-d70431d5bac0";
-            break;
-        case "forever-we-sing-hallelujah":
-            processedSong = "forever-we-sing-halleluia";
-            break;
-        case "heart-of-god":
-            processedSong += "-7a2a9346-717e-4ea1-88bc-7929bb275c74";
-            break;
-        }
-    if (choice.song === "Angels We Have Heard On High" && choice.album === "We Sing To You, Jesus (Carols For Worship)") {
-        processedSong =+ "-1bc7c9c0-6aa7-4ca5-b0c2-ce235d4082be";
-        }
-    var url = `https://app.theworshipinitiative.com/songs/${processedSong}/instrument/tutorials#song`;
-    document.querySelector("a").setAttribute("href", url);
+    
+    function processURL(choice) {
+        let processedSong = choice.song.replace(/[-\/:',()[\] ]/g, '-').replace(/-+/g, '-').replace(/^-+|-+$/g, '').toLowerCase();
+        switch (processedSong) {
+            case "all-glory-be-to-christ":
+                processedSong += "-8aeb1bd6-6af8-4a8c-90e7-b8dfe768eec3";
+                break;
+            case "there-s-a-river":
+                processedSong += "-31a29457-cb2c-4538-a81b-d70431d5bac0";
+                break;
+            case "forever-we-sing-hallelujah":
+                processedSong = "forever-we-sing-halleluia";
+                break;
+            case "heart-of-god":
+                processedSong += "-7a2a9346-717e-4ea1-88bc-7929bb275c74";
+                break;
+            }
+            if (choice.song === "Angels We Have Heard On High" && choice.album === "We Sing To You, Jesus (Carols For Worship)") {
+                processedSong =+ "-1bc7c9c0-6aa7-4ca5-b0c2-ce235d4082be";
+                }
+            var url = `https://app.theworshipinitiative.com/songs/${processedSong}/instrument/tutorials#song`;
+        document.querySelector("a").setAttribute("href", url);
+    }
 }
