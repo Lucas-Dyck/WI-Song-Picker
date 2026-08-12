766f42a0f8b6b19fce93f8ad455770b5734495d4
Lucas-Dyck <lucasedyck@gmail.com>
2026-08-12 16:19:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,11 @@ function handleClick(songLibrary) {
             if (choice.song === "Angels We Have Heard On High" && choice.album === "We Sing To You, Jesus (Carols For Worship)") {
                 processedSong += "-1bc7c9c0-6aa7-4ca5-b0c2-ce235d4082be";
                 }
-            if (choice.song === "How Great Thou Art" && choice.album === "Worship Initiative Hymns Volume 2") {
+            if (choice.song === "How Great Thou Art" && choice.album === "The Worship Initiative Hymns Volume 2") {
                 processedSong += "-edde77a4-580b-4f07-ad00-0abc3f42f5c2";
+            }
+            if (choice.song === "All Hail King Jesus" && choice.album === "The Worship Initiative Volume 18") {
+                processedSong += "-aa0a8bbf-6497-479d-bc25-5829a942d6bf";
             }
             var url = `https://app.theworshipinitiative.com/songs/${processedSong}/instrument/tutorials#song`;
         document.querySelector("a").setAttribute("href", url);
