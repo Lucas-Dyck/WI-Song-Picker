1c2b632a72ef94fd68b2054e4dc5dd1024b9ecb3
Lucas-Dyck <lucasedyck@gmail.com>
2026-09-05 13:24:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function handleClick(songLibrary) {
             if (choice.song === "All Hail King Jesus" && choice.album === "The Worship Initiative Volume 18") {
                 processedSong += "-aa0a8bbf-6497-479d-bc25-5829a942d6bf";
             }
-            if (choice.song === "Is He Worthy" && choice.album === "Watermark Worship Channel") {
+            if (choice.song === "O Holy Night" && choice.album === "We Sing to You, Jesus (Carols for Worship)") {
                 processedSong += "-a716d25a-985d-4e88-9bda-8edb35429be6";
             }
             var url = `https://app.theworshipinitiative.com/songs/${processedSong}/instrument/tutorials#song`;
